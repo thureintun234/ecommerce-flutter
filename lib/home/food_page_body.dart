96a23d890dd45aa44bff83da61f6bbc63e32a796
thureintun234 <thurein12456@gmail.com>
2022-06-05 19:41:54 +0630

    first

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,139 @@
+import 'package:ecommerce/utils/colors.dart';
+import 'package:ecommerce/widgets/big_text.dart';
+import 'package:ecommerce/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/icon_and_text.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController _pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  double _scaleFactor = 0.8;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPageValue = _pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 320,
+        child: PageView.builder(
+            controller: _pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItem(position);
+            }));
+  }
+
+  Widget _buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currentPageValue.floor()) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currentScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/image/food0.png"),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                  padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BigText(text: "Chinese Side"),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: List.generate(
+                              5,
+                              (index) => Icon(Icons.star,
+                                  color: AppColors.mainColor, size: 15),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          SmallText(text: "4.5"),
+                          SizedBox(width: 10),
+                          SmallText(
+                            text: "1287",
+                          ),
+                          SizedBox(width: 10),
+                          SmallText(text: "comments"),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          IconAndText(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor1,
+                          ),
+                          SizedBox(width: 10),
+                          IconAndText(
+                            icon: Icons.location_on,
+                            text: "1.7km",
+                            iconColor: AppColors.mainColor,
+                          ),
+                          SizedBox(width: 10),
+                          IconAndText(
+                            icon: Icons.access_time_rounded,
+                            text: "32min",
+                            iconColor: AppColors.iconColor2,
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BigText extends StatelessWidget {
+  Color color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+  BigText(
+      {Key? key,
+      this.color = const Color(0xFF332d2b),
+      required this.text,
+      this.overFlow = TextOverflow.ellipsis,
+      this.size = 20})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overFlow,
+      style: TextStyle(fontSize: size, color: color, fontFamily: 'Roboto'),
+    );
+  }
+}
